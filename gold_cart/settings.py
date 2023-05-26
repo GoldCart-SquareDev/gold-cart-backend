4143fefb14569c35c63b22f1e4f307e61f6756ff
yaw.o.k <obengky72@gmail.com>
2023-05-26 17:26:04 +0000

    Add users app

--- gold_cart/settings.py
+++ gold_cart/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "store.apps.StoreConfig",
+    "users",
     
     "django.contrib.admin",
     "django.contrib.auth",
@@ -128,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ]
